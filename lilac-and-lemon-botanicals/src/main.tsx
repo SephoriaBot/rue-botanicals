bbb1773dcd1d854ef70d298791e094bf5d032474
SephoriaBot <withallofthisimdone@gmail.com>
2026-09-02 18:02:34 -0400

    test

--- lilac-and-lemon-botanicals/src/main.tsx
+++ lilac-and-lemon-botanicals/src/main.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ClerkProvider } from '@clerk/react-router';
 import App from './App';
 import './index.css';
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing VITE_CLERK_PUBLISHABLE_KEY');
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-      </ClerkProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
