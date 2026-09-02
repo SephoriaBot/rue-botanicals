c9cbf4993a2d7a3f5d78359ed0e4fbb5fccc0a21
SephoriaBot <withallofthisimdone@gmail.com>
2026-09-02 17:57:47 -0400

    again

--- lilac-and-lemon-botanicals/src/pages/TesterLogin.tsx
+++ lilac-and-lemon-botanicals/src/pages/TesterLogin.tsx
@@ -3,7 +3,11 @@ import { SignIn } from '@clerk/react-router';
 export default function TesterLogin() {
   return (
     <div className="tester-login-page">
-      <SignIn />
+      <SignIn
+        routing="path"
+        path="/tester/login"
+        fallbackRedirectUrl="/tester"
+      />
     </div>
   );
 }
